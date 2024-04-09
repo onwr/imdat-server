c6633655d269e101b3c58bdd026d49227e1e79ba
Onur <121825730+onwr@users.noreply.github.com>
2024-04-09 23:43:55 +0300

    sa

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
-import app from './index.js'
+const express = require("express");
+const http = require("http");
+require("dotenv");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 3000
+const app = express();
 
-app.listen(PORT, () => console.log(`Server running at port ${ PORT }`))
+app.use(cors());
 
-export default app
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Kullanıcı bağlandı", socket.id);
+
+  socket.on("ihbar", (donen) => {
+    console.log("Yeni ihbar", donen);
+
+    io.emit("imdat", donen);
+  });
+});
+
+server.listen(process.env.PORT || 1881, () => {
+  console.log("Server ayağa kaldırıldı.");
+});
