c2dd4a7a3fcb25700c9f84f743efa985485101fb
Onur <121825730+onwr@users.noreply.github.com>
2024-04-09 23:31:54 +0300

    sa

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+import "dotenv/config";
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Kullanıcı bağlandı", socket.id);
+
+  socket.on("ihbar", (donen) => {
+    console.log("Yeni ihbar", donen);
+
+    io.emit("imdat", donen);
+  });
+});
+
+server.listen(process.env.PORT || 1881, () => {
+  console.log("Server ayağa kaldırıldı.");
+});
